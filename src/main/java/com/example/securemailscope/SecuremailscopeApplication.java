49150d9579eb8ad61e53e5757ead52d0c7028a78
ganeshparmarit-lgtm <ganeshparmar.it@gmail.com>
2026-09-12 01:34:54 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/example/securemailscope/SecuremailscopeApplication.java
@@ -0,0 +1,13 @@
+package com.example.securemailscope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuremailscopeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecuremailscopeApplication.class, args);
+    }
+
+}
